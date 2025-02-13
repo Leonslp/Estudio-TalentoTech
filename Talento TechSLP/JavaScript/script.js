2e20130caffdd9b20b2fa71b3e0a347814797f4d
Santiago Leon <93327269+Leonslp@users.noreply.github.com>
2025-02-12 20:49:13 -0500

    Add files via upload

--- Talento TechSLP/JavaScript/script.js
+++ Talento TechSLP/JavaScript/script.js
@@ -1,3 +1,4 @@
+/*
 function operar(){
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
@@ -15,3 +16,35 @@ function operar(){
         alert(`La resta de los numeros es: ${(num1/num2)}`)
     }
 }   
+*/
+function operar() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var eleccion = document.getElementById("operacion").value;
+    
+
+    if (isNaN(num1) || isNaN(num2)) {
+        resultado.innerHTML = `<b style="color: crimson;"> los campos estan vacios </b>`;
+        return;
+    }
+    switch (eleccion) {
+        case "suma":
+            resultado.innerHTML = `el resultado de la suma es: ${num1 + num2}`;
+            break;
+        case "resta":
+            resultado.innerHTML = `el resultado de la resta es: ${num1 - num2}`;
+            break;
+        case "multiplicar":
+            resultado.innerHTML = `el resultado de la multiplicacion es: ${num1 * num2}`;
+            break;
+        case "division":
+            if (num2 === 0){
+                resultado.innerHTML = `no se puede dividir por 0`;
+            } else {
+                resultado.innerHTML = `el resultado de la division es: ${num1 / num2}`;
+            }
+            break;
+        default:
+            resultado.innerHTML = `<b style="color: crimson;"> Seleccione la operacion </b>`;
+    }
+}
