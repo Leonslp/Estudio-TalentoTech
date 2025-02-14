e447dbfc7555fad81e90d55b8601568155af12ae
Santiago Leon <93327269+Leonslp@users.noreply.github.com>
2025-02-13 21:27:17 -0500

    Add files via upload

--- Talento TechSLP/site/script.js
+++ Talento TechSLP/site/script.js
@@ -1,3 +1,28 @@
-function info(){
+function info() {
     alert('Bienvenido 👈(ﾟヮﾟ👈)')
+}
+
+//Crear funcionalidad para desaparecer el formulario
+
+function ver(id) {
+    //Metodo
+    document.getElementById(id).style.display = "block"
+}
+
+function cerrar(id) {
+    document.getElementById(id).style.display = "none"
+}
+
+//Crear funcion para visualizar el boton Arriba
+let up = document.querySelector('#arriba')
+
+window.addEventListener('scroll', btn_arriba)
+
+function btn_arriba() {
+    if (window.scrollY > 200){
+        console.log('Estas ubicado por encima de los 200px');
+    arriba.classList.add('activar');
+    } else {
+        arriba.classList.remove('activar')
+    }
 }
